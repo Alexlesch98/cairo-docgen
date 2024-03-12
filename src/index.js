bd2eb31b5301a520429a50284d134f50cdc26c38
Alexlesch98 <160536953+Alexlesch98@users.noreply.github.com>
2024-03-12 12:46:46 +0100

    Update index.js
    1. Used fs.promises for asynchronous file operations to improve readability and maintainability.
    2. Replaced the callback-based fs.writeFile with await fs.writeFile for better asynchronous handling.
    3. Wrapped the content reading and documentation generation in an async function to leverage await.
    4. Moved the try-catch block to handle potential errors during documentation generation.
    5. Made sure to use the correct path separator in path.resolve for cross-platform compatibility.

--- src/index.js
+++ src/index.js
@@ -1,40 +1,38 @@
 #!/usr/bin/env node
 
 const program = require('commander');
-const fs = require('fs');
+const fs = require('fs').promises;
 const Handlebars = require('handlebars');
 const path = require('path');
 const parser = require('./parser');
 
 const MD_TEMPLATE = Handlebars.compile(
-  fs.readFileSync(path.resolve(__dirname, "./templates/md.hbs"), {encoding:'utf8', flag:'r'})
+  fs.readFileSync(path.resolve(__dirname, './templates/md.hbs'), 'utf8')
 );
 
+async function generateDocs(content, outPath) {
+  try {
+    const { constructor, externalFunctions, viewFunctions, events, l1Handlers } = parser.parse(content);
 
-function generateDocs(content, outPath) {
-  const { constructor, externalFunctions, viewFunctions, events, l1Handlers } = parser.parse(content);
-  
-  const formattedFileContent = MD_TEMPLATE({ constructor, externalFunctions, viewFunctions, events, l1Handlers });
-  fs.writeFile(outPath, formattedFileContent, (error) => {
-      if (error) {
-        console.error(error);
-      } else {
-        console.log(`File written successfully to ${outPath}`);
-      }
-    });
+    const formattedFileContent = MD_TEMPLATE({ constructor, externalFunctions, viewFunctions, events, l1Handlers });
+
+    await fs.writeFile(outPath, formattedFileContent);
+    console.log(`File written successfully to ${outPath}`);
+  } catch (error) {
+    console.error(error);
+  }
 }
-
 
 program
   .description('Generate documentation for Cairo smart contracts')
   .arguments('<inputFile> [outputDir]')
   .option('-o, --outputDir <outputDir>', 'Directory to write the generated documentation to', '.')
-  .action((inputFile, outputDir, defaults) => {
+  .action(async (inputFile, outputDir, defaults) => {
     outputDir = outputDir ?? defaults.outputDir;
-    const sourceContent = fs.readFileSync(inputFile, {encoding:'utf8', flag:'r'});
+    const sourceContent = await fs.readFile(inputFile, 'utf8');
     const outputFile = path.resolve(outputDir, `${path.basename(inputFile)}.md`);
-    
-    generateDocs(sourceContent, outputFile);
+
+    await generateDocs(sourceContent, outputFile);
   });
 
 program.parse(process.argv);
